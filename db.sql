bb04910ffbdc492838d406c7733a394b885ef4a0
Chan Li Liao <cliao119@gmail.com>
2021-09-21 20:05:01 -0400

    db created

---
+++ db.sql
@@ -0,0 +1,26 @@
+-- \? list of commends
+-- \l list of database
+-- \c db_name connect to db
+-- \d show the tables
+-- \d table_name shows the actual table
+-- CREATE DATABASE db_name; create db
+-- DROP DATABASE db_name; delete db
+-- CREATE TABLE table_name {
+  -- col_name datatype
+-- }
+-- DROP TABLE table_name 
+------ delete table 
+-- ALTER TABLE table_name ADD/DROP COLUMN col_name datatype
+----- choose the table you want to alter
+-- INSERT INTO table_name (col1, col2,..) values () 
+----- insert data
+-- SELECT * /(col1, col2) FROM table_name 
+----- view the table * means all or just col 
+
+CREATE TABLE users (
+  user_id SERIAL primary key,
+  first_name VARCHAR(50) not null,
+  last_name VARCHAR(50) not null,
+  email CITEXT not null unique,
+  password text not null
+);
